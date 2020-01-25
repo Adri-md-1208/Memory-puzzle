45ec8aebe64dd99db16ded9ba550492673e1ee18
Adrian <adri.md.2001@gmail.com>
2020-01-25 20:45:50 +0100

    main finished

--- Games/Memory/main.py
+++ Games/Memory/main.py
@@ -31,11 +31,6 @@ BGCOLOR = GRAY
 HIGHLIGHTCOLOR = CYAN # The color around every box
 BOXCOLOR = BLUE
 
-# DISPLAY
-#################################################################################################################################
-DISPLAY = pygame.display.set_mode((640, 480))
-pygame.display.set_caption('Memory puzzle!')
-
 # SPRITES 
 #################################################################################################################################
 POKEBALL    = pygame.image.load('C:/Users/Adrián/Desktop/Python-games/Games/Memory/sprites/pokeball.png')
@@ -62,10 +57,8 @@ def main():
 
     mousex = 0
     mousey = 0
-
     mainBoard = getRandomizedBoard() # in line 
     revealedBoxes = setRevealedBoxes(False) # in line
-
     firstSelection = None # Boolean type variable that set if the first box was revealed
 
     DISPLAY.fill(BGCOLOR)
@@ -82,12 +75,41 @@ def main():
                 pygame.quit()
                 sys.exit()
             elif event.type == MOUSEMOTION:
-                mousex, mousey = event.pos
+                mousex, mousey = event.pos # Get the mouse position
             elif event.type == MOUSEBUTTONUP:
-                mousex, mousey = event.pos
+                mousex, mousey = event.pos # Get the mouse position when the mouse is clicked
                 mouseClicked = True
             
             boxx, boxy = getBoxAtPixel(mousex, mousey)
             if boxx != None and boxy != None: # When the mouse is over a box
                 if not revealedBoxes([boxx][boxy]):
                     drawHighlightBox(boxx, boxy)
+                if not revealedBoxes([boxx][boxy]) and mouseClicked:
+                    revealedBoxes(mainBoard, [(boxx, boxy)]) # in line
+                    revealedBoxes[boxx][boxy]
+                    if firstSelection == None:
+                        firstSelection = (boxx, boxy) # Set the box as the first selection
+                    else: # That is the second selection
+                        firstIcon = getIcons(mainBoard, firstSelection[0], firstSelection[1]) # in line
+                        secondIcon = getIcons(mainBoard, boxx, boxy) # in line
+                        if firstIcon != secondIcon:
+                            pygame.time.wait(1000) # Retard of 1 second (1000 milliseconds)
+                            coverBoxesAnimation(mainBoard, [(firstSelection[0], firstSelection[1]), (boxx, boxy)])
+                            revealedBoxes(firstSelection[0], firstSelection[1]) = False
+                            revealedBoxes(boxx, boxy) = False
+                        elif hasWon(revealedBoxes):
+                            gameWonAnimation(mainBoard) # in line
+                            pygame.time.wait(2000)
+
+                            mainBoard = getRandomizedBoard() # Restart the game 
+                            revealedBoxes = setRevealedBoxes(False) # in line
+
+                            drawBoard(mainBoard, revealedBoxes) # Show the board unrevealed
+                            pygame.display.update()
+                            pygame.time.wait(1000)
+
+                            startGameAnimation(mainBoard)
+                        firstSelection = None
+
+                        pygame.display.update()
+                        FPSCLOCK.tick(FPS)
