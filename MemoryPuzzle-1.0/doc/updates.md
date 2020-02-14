392d00fc0f8988653f149aca3cf26d56564e6fdc
Adrian <adri.md.2001@gmail.com>
2020-02-14 14:19:37 +0100

    More files

---
+++ MemoryPuzzle-1.0/doc/updates.md
@@ -0,0 +1,8 @@
+# Next Updates
+## Actual version : 1.0
+- [x] Dist package
+- [ ] Game clock
+- [ ] Game menu
+- [ ] Score files
+- [ ] More animations
+- [ ... ] Uncoming versions
---
+++ MemoryPuzzle-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup (
+
+    name = "MemoryPuzzle",
+    version = "1.0",
+    description = "A memory puzzle build on python",
+    author = "Adri-md-1208", 
+    author_email = "a.morales.2019@alumnos.urjc.es",
+    url = "https://github.com/Adri-md-1208/Memory-puzzle",
+    packages = ["memoryPuzzle", "sprites"],
+    license = "GPLv3",
+    
+)
