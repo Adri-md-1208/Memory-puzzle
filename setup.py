5b2f02296550eec83fb1c70ef46f06b64c5ca739
Adrian <adri.md.2001@gmail.com>
2020-02-20 20:21:44 +0100

    Making a dist package

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup (
 
-    name = "MemoryPuzzle",
+    name = "MemoryPuzzle-1.0",
     version = "1.0",
     description = "A memory puzzle build on python",
+    long_description = long_description,
     author = "Adri-md-1208", 
     author_email = "a.morales.2019@alumnos.urjc.es",
     url = "https://github.com/Adri-md-1208/Memory-puzzle",
-    packages = [
-                "MemoryPuzzle-1.0",
-                "MemoryPuzzle-1.0.memoryPuzzle",
-                "MemoryPuzzle-1.0.sprites"
-                ],
+    install_requeries = ["pygame"],
+    packages = ["MemoryPuzzle-1.0"],
     license = "GPLv3",
     
 )
